669124ef60e1032bf8250994edf5ab010a71a179
olyeesa <shibayeva@gmail.com>
2023-12-15 20:03:20 +0300

    lesgo

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,13 @@
 let express = require('express');
-let adminController = require(".../controllers/adminontroller.js");
+let adminController = require(".../controllers/adminController.js");
 let router = express.Router();
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({extended : false});
 let jsonParser = express.json();
 
-<<<<<<< HEAD
-//Комментарий из ветки моделей
-=======
 
->>>>>>> 670c255300ebda3f3422dc47c582dc04399b5a3f
 router.get('/',adminController.getArticleAll);
-router.get('/: idArticle',adminController.getArticle);
+router.get('/:idArticle',adminController.getArticle);
 router.get('/addArticle',adminController.addArticle);
 router.get('/editArticle',adminController.editArticle);
 router.get('/deleteArticle',adminController.deleteArticle);
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,9 @@ let express = require('express');
 let router = express.Router();
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({extended : false});
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
+let jsonParser = express.json();
+let adminController = require(".../controllers/homeController.js");
 
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+router.get('/',homeController.getIndex);
 
-let index = require('./routes/index');
-app.use('/', index);
-
-let article = require('./routes/article');
-app.use('/article', article);
-
-let admin = require('./routes/admin');
-app.use('/admin', admin);
-
-app.listen(3000);
+module.exports = router;
